1cb2b78796fb364f371ba0a1e24290ba4c15d9dd
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-07-24 14:03:45 -0500

    Comments

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -11,9 +11,9 @@ public class ConsoleExercises {
 //        1. Prompt a user to enter a integer and store that value in an `int` variable using the `nextInt` method.
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your favorite integer: ");
-        int favInt = sc.nextInt();
-        System.out.println("Your favorite int is " + favInt + "\n");
+//        System.out.print("Enter your favorite integer: ");
+//        int favInt = sc.nextInt();
+//        System.out.println("Your favorite int is " + favInt + "\n");
 //
 //        What happens if you input something that is not an integer?
 //        ANS: Runtime error!
@@ -21,11 +21,11 @@ public class ConsoleExercises {
 //        2. Prompt a user to enter 3 words and store each of them in a separate variable, then display them back, each on a newline.
 //
 
-        System.out.print("Enter three words: ");
-        String wordOne = sc.next();
-        String wordTwo = sc.next();
-        String wordThree = sc.next();
-        System.out.printf("You entered: \n%s\n%s\n%s\n\n", wordOne, wordTwo, wordThree);
+//        System.out.print("Enter three words: ");
+//        String wordOne = sc.next();
+//        String wordTwo = sc.next();
+//        String wordThree = sc.next();
+//        System.out.printf("You entered: \n%s\n%s\n%s\n\n", wordOne, wordTwo, wordThree);
 
 //        What happens if you enter less than 3 words?
 //        ANS: Waits for you to enter the third word on a new line.
@@ -45,10 +45,10 @@ public class ConsoleExercises {
 //
 //        4. Rewrite the above example using the `.nextLine` method.
 
-        sc.nextLine();
-        System.out.print("Please enter a complete sentence: ");
-        String compSentence = sc.nextLine();
-        System.out.printf("You entered: \"%s\"\n\n", compSentence);
+//        sc.nextLine();
+//        System.out.print("Please enter a complete sentence: ");
+//        String compSentence = sc.nextLine();
+//        System.out.printf("You entered: \"%s\"\n\n", compSentence);
 
 
 
@@ -60,59 +60,59 @@ public class ConsoleExercises {
 //                - Assume that the rooms are perfect rectangles.
 //                - Assume that the user will enter valid numeric data for length and width.
 
-        System.out.print("Enter the length of the classroom (in feet): ");
-        float classLength = sc.nextFloat();
-        System.out.print("Enter the width of the classroom (in feet): ");
-        float classWidth = sc.nextFloat();
-        System.out.print("Enter the height of the classroom (in feet): ");
-        float classHeight = sc.nextFloat();
-        System.out.printf("\nThe dimensions of the classroom are: %fft x %fft\n", classLength, classWidth);
+//        System.out.print("Enter the length of the classroom (in feet): ");
+//        float classLength = sc.nextFloat();
+//        System.out.print("Enter the width of the classroom (in feet): ");
+//        float classWidth = sc.nextFloat();
+//        System.out.print("Enter the height of the classroom (in feet): ");
+//        float classHeight = sc.nextFloat();
+//        System.out.printf("\nThe dimensions of the classroom are: %fft x %fft\n", classLength, classWidth);
 
 //
 //        2. Display the area and perimeter of that classroom.
 
-        float classArea = classLength * classWidth;
-        float classPerimeter = (2 * classLength) + (2 * classWidth);
+//        float classArea = classLength * classWidth;
+//        float classPerimeter = (2 * classLength) + (2 * classWidth);
 
-        System.out.printf("The area of the classroom is %fsqft\nThe perimeter of the classroom is %fft\n", classArea, classPerimeter);
+//        System.out.printf("The area of the classroom is %fsqft\nThe perimeter of the classroom is %fft\n", classArea, classPerimeter);
 
 //        == Bonus ==
 
 //        - Accept decimal entries  ----> DONE
 
 //        - Calculate the volume of the rooms  ----> DONE
-        double classVolume = classArea * classHeight;
-
-        System.out.printf("The volume of the classroom is %f cubic ft\n\n", classVolume);
+//        double classVolume = classArea * classHeight;
+//
+//        System.out.printf("The volume of the classroom is %f cubic ft\n\n", classVolume);
 
 //        === EXTRA EXTRA BONUSES ===
 
 //        BONUS 1
 //        Prompt the user to enter a favorite quote
-        sc.nextLine();
-        System.out.print("Enter your favorite quote: ");
-        String favQuote = sc.nextLine();
-//        Output the quote
-        System.out.println("\"" + favQuote + "\"\n");
-//        Ask them to enter how many words are in the quote
-        System.out.print("How many words are in this quote? ");
-        int numWords = sc.nextInt();
-//        Output the number they entered
-        System.out.println("You entered " + numWords);
+//        sc.nextLine();
+//        System.out.print("Enter your favorite quote: ");
+//        String favQuote = sc.nextLine();
+////        Output the quote
+//        System.out.println("\"" + favQuote + "\"\n");
+////        Ask them to enter how many words are in the quote
+//        System.out.print("How many words are in this quote? ");
+//        int numWords = sc.nextInt();
+////        Output the number they entered
+//        System.out.println("You entered " + numWords);
 //
 //        BONUS 2
 //        Prompt the user to enter a list of top three favorite foods separated by only spaces
-        System.out.println("\n\nEnter a list of your top three favorite foods (separate with spaces, NOT commas): ");
-        String firstFood = sc.next();
-        String secondFood = sc.next();
-        String thirdFood = sc.next();
+//        System.out.println("\n\nEnter a list of your top three favorite foods (separate with spaces, NOT commas): ");
+//        String firstFood = sc.next();
+//        String secondFood = sc.next();
+//        String thirdFood = sc.next();
 
 //        Use the printf() to output there three top foods with the format:
 //        1) FirstFood
 //        2) SecondFood
 //        3) ThirdFood
 
-        System.out.printf("Your top three favorite foods:\n1) %s\n2) %s\n3) %s\n\n", firstFood, secondFood, thirdFood);
+//        System.out.printf("Your top three favorite foods:\n1) %s\n2) %s\n3) %s\n\n", firstFood, secondFood, thirdFood);
 //
 //        BONUS 3
 //        Prompt the user to enter a grocery list of three items
